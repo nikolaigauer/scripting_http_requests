20f12c0e648be3ae7ab838ff06e670ce7c43d92d
Nikolai <nikolaigauer@gmail.com>
2017-07-03 19:36:49 +0000

    adding step 3

--- step3.js
+++ step3.js
@@ -1,37 +1,47 @@
 // while https is built-in to Node, it is a module, so it must be required
 var https = require('https');
 
-// the host can be thought of as the domain name you want to read from,
-// and the path is the resource - '/' is the root path, but if you wanted to read a
-// particular resource (like '/login/index.html'), that would be defined in the path
-function getAndPrintHTMLChunks () {
-
-  var requestOptions = {
+var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
-  /* Add your code here */
 
+
+
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+function getAndPrintHTML (options) {
+  var options = {
+  host: requestOptions.host,
+  path: requestOptions.path
+};
+
+
+   /* Add your code here */
 
 
 // notice that https.get takes a callback with one parameter -
 // response, which is a Stream that represents the HTTP response
-https.get(requestOptions, function (response) {
+https.get(options, function (response) { // changed from requestOptions to options
+  var str = '';
 
   // set encoding of received data to UTF-8
   response.setEncoding('utf8');
 
   // the callback is invoked when a `data` chunk is received
-  response.on('data', function (data) {
-    console.log('Chunk Received. Length:', data.length);
+  response.on('data', function (chunk) {
+    str += chunk;
+    console.log('Chunks Received and appended to each other: ');
   });
 
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
   response.on('end', function() {
-    console.log('Response stream complete.');
+    console.log(str);
   });
 
 });
 }
-return getAndPrintHTMLChunks()
+
+return getAndPrintHTML()
